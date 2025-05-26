ff732ab742fe5672909db92432679e04405e7d47
vaishnavirayee <vaishnavirayee35@gmail.com>
2025-05-26 20:23:00 +0530

    Add files via upload

---
+++ rfid_arduino.ino
@@ -0,0 +1,25 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 rfid(SS_PIN, RST_PIN);
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();      
+  rfid.PCD_Init();  
+  Serial.println("Scan a Tag...");
+}
+
+void loop() {
+  if (!rfid.PICC_IsNewCardPresent()) return;
+  if (!rfid.PICC_ReadCardSerial()) return;
+
+  Serial.print("UID: ");
+  for (byte i = 0; i < rfid.uid.size; i++) {
+    Serial.print(rfid.uid.uidByte[i], HEX);
+  }
+  Serial.println();
+  delay(1000);
+}
